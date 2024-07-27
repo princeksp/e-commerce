35719c3bbd0384e61e9af9c31bda9fa5dd4cd358
Raj Kumar Mishra <119384637+princeksp@users.noreply.github.com>
2024-07-27 10:39:21 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,43 @@
-// src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">E-commerce Store</Link>
-      </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <HeaderContainer>
+      <Logo to="/">MyStore</Logo>
+      <Nav>
+        <SearchBar placeholder="Search products..." />
+        <Link to="/cart">Cart</Link>
+      </Nav>
+    </HeaderContainer>
   );
 };
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+`;
+
+const Logo = styled(Link)`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+const SearchBar = styled.input`
+  padding: 0.5rem;
+  margin-right: 1rem;
+`;
 
 export default Header;
