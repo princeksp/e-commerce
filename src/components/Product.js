9783271c68bfb7e3527a3eb1b779c2c7a69dbfac
Raj Kumar Mishra <119384637+princeksp@users.noreply.github.com>
2024-07-27 10:40:04 +0530

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,33 @@
-// src/components/Product.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Product = ({ product }) => {
   return (
-    <div className="product">
-      <img src={product.image} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>${product.price}</p>
-      <button>Add to Cart</button>
-    </div>
+    <ProductCard>
+      <Link to={`/product/${product._id}`}>
+        <img src={product.image} alt={product.name} />
+        <h3>{product.name}</h3>
+        <p>${product.price}</p>
+      </Link>
+    </ProductCard>
   );
 };
+
+const ProductCard = styled.div`
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  h3 {
+    margin: 0.5rem 0;
+  }
+  p {
+    color: #333;
+  }
+`;
 
 export default Product;
